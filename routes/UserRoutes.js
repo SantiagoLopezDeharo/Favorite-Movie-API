aae654df765220f8d1160388be312a1f444bd79c
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2024-09-12 12:06:21 -0300

    Commente la ruta para listar a los usuarios

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getAllUsers, addUser, auth, authenticateToken, invalidateToken } = require('../controllers/UserController');
 const router = express.Router();
 
-router.get('/users', authenticateToken, getAllUsers);
+// router.get('/users', authenticateToken, getAllUsers); // This is a test route to see the users listed in the system, only for development runtime
 router.post('/users', addUser);
 router.post('/login', auth);
 router.delete("/logout", invalidateToken);
